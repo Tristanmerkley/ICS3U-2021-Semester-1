e033eda6f511d28fca925aa465713ac103eebcc6
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-09 13:22:19 -0400

    Test 2

--- Unit One - Introduction to Computer Science/src/week1/ExampleOne.java
+++ Unit One - Introduction to Computer Science/src/week1/ExampleOne.java
@@ -3,5 +3,8 @@ package week1;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("Welcome to ICS3U - AP!"); // system.out.println displays information text to the screen and hits enter
+
+        // anthing in quotes is considered a string literal
+        // a string is a sequence of characters (strings are in double quotes)
     }
 }
