182093dfb1031fa2f1dd9fd6f2e30441d63a5783
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-12 20:47:27 -0400

    Testing

--- Unit One - Introduction to Computer Science/src/week1/TestingStuff.java
+++ Unit One - Introduction to Computer Science/src/week1/TestingStuff.java
@@ -2,6 +2,16 @@ package week1;
 
 public class TestingStuff {
     public static void main(String[] args) {
-        
+        int x = 8 % 5;
+        int a = 5;
+        int b = 8;
+        int c = 3;
+        System.out.println(a + b / c * 2);
+
+        int num = 75;
+        int onesDigit = num % 10;
+        int tensDigit = num / 10;
+        System.out.print(onesDigit);
+        System.out.print(tensDigit);
     }
 }
