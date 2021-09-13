6d9bc4cc4b9b7ed831406bfb85451346a081c652
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-13 12:59:02 -0400

    final modifier and Type Casting

---
+++ Unit One - Introduction to Computer Science/src/week2/ExampleSeven.java
@@ -0,0 +1,19 @@
+package week2;
+
+/**
+ * Final modifier, modifies the variable so that it is a constant
+ */
+
+public class ExampleSeven {
+    public static void main(String[] args) {
+        final int x = 7;
+        //x = 8; // cannot change a final variable
+
+
+        final int y;
+        y = 8;
+        // y = 2; // cannot change a final variable
+
+        final int NUMBER_OF_STUDENTS = 13; // naming convention for constants is UPPER_CASE _ for spaces
+    }
+}
