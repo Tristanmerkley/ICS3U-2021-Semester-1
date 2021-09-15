f324a21fb2ad92066cfe3fa1e98e02241824557e
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-14 20:13:12 -0400

    Unit One Homework

---
+++ Unit One - Introduction to Computer Science/src/week2/HW1.java
@@ -0,0 +1,89 @@
+package week2;
+
+public class HW1 {
+    public static void main(String[] args) {
+        // number one
+        /*double number = 7.3; 
+
+        double numsqrd = number * number;
+
+        System.out.println(number + "x" + number + "=" + numsqrd);
+
+        // number two
+        int totalscore = 20;
+        int scoredamount = 15;
+
+        double total = ((double)scoredamount / totalscore) * 100;
+
+        System.out.println("You got " + total + "%");
+
+        // number three
+
+        int num = 5678;
+
+        int onesDigit = num % 10;
+        int tensDigit = (num % 100) / 10;
+        int hundredsDigit = (num % 1000) / 100;
+        int thousandsDigit = (num % 10000) / 1000;
+
+        // number four
+
+        int acceleration = 14;
+        int time = 17;
+        int intVelocity = 80;
+
+        int finalVelocity = intVelocity + (acceleration * time);
+
+        // number five
+
+        int r = 5;
+        int rsqrd = r * r;
+        double area = Math.PI * (double) rsqrd;
+        System.out.println(area);
+
+        // number six
+
+        int r = 6;
+        int rcubd = r * r * r;
+        double volumeSphere = (double) 4 / 3 * Math.PI * rcubd;
+        System.out.println(volumeSphere);
+
+        // number seven
+
+        int a = 2;
+        int b = 4;
+        int c = 6;
+        int x = 8;
+        int xsqrd = x * x;
+        double y = a + xsqrd + b * x + c;
+        System.out.println(y);
+
+        // number eight
+
+        int x1 = 5;
+        int x2 = 7;
+        int y1 = 3;
+        int y2 = 9;
+        double slope = (y1 - y2) / (x1 - x2);
+        System.out.println(slope);
+
+        // number nine
+
+        int p = 3, n = 5, d = 6, q = 2, l = 6, t = 5;
+        double totalCash = p * 0.01 + n * 0.05 + d * 0.1 + q * 0.25 + l * 1 + t * 2;
+        System.out.println(totalCash);
+
+        // number ten
+
+        int b = -5;
+        int a = 1;
+        int c = 6;
+        int bsqrd = b * b;
+
+        double posX = (-b + Math.sqrt(bsqrd - 4 * a * c)) / 2 * a;
+        double negX = (-b - Math.sqrt(bsqrd - 4 * a * c)) / 2 * a;
+
+        System.out.println(posX + "OR" + negX);
+*/
+    }
+}
