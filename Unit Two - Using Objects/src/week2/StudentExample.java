ed0dbce6b60c0260c07f22ed55128a6bbad15038
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-16 13:00:36 -0400

    Students class and example (calling non-static and static)

---
+++ Unit Two - Using Objects/src/week2/StudentExample.java
@@ -0,0 +1,24 @@
+package week2;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        // ritchie is a variable to an instance of the class student
+        // ritchie is an object (instances of a class)
+        Student ritchie = new Student("Ritchie", "45646", 10);
+        Student yola = new Student("Yola", "123456", 10);
+
+        // ritchie and yola are references to 2 different students
+
+        Student rastin = ritchie;
+
+
+        rastin.increaseGrade();
+
+        ritchie = null;
+        //Student tristan; tristan is currently the null reference
+
+        //ritchie.increaseGrade(); // throws a java.lang.NullPointerException because ritchie is null
+
+
+    }
+}
