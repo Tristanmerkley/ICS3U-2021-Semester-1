1632bfd9a198f4ef05d8c0c12c93c3e6a0234b42
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-27 12:48:33 -0400

    Free response questions

--- Unit Two - Using Objects/src/week3/ExtraQuestions.java
+++ Unit Two - Using Objects/src/week3/ExtraQuestions.java
@@ -4,8 +4,34 @@ public class ExtraQuestions {
     public static void main(String[] args) {
         questionOne();
         questionTwo();
-        String str = question5("this is a Sentence");
+        String str = question5("this is a Sentence.");
+        str = question6("This is a Sentence.", "ent");
         
+        double slope = question8(1, 2, 4, 5); // (1, 2) and (4, 5)
+        
+        System.out.println(question10("This is a sentence."));
+    }
+    private static String question10(String string) {
+        int index = (int)(Math.random()*string.length());
+        String first = string.substring(0, index);
+        String last = string.substring(index + 1);
+
+        return first + last;
+    }
+    private static double question8(int x1, int y1, int x2, int y2) {
+        // slope = (deltaY / deltaX)
+
+        int deltaY = y2 - y1;
+        int deltaX = x2 - x1;
+
+        double slope = (double) deltaY / deltaX;
+        return slope;
+    }
+    private static String question6(String str, String substr) {
+        int index = str.indexOf(substr);
+        String first = str.substring(0, index);
+        String last = str.substring(index + str.length());
+        return first + last;
     }
     private static String question5(String string) {
         int index = string.indexOf("e");
