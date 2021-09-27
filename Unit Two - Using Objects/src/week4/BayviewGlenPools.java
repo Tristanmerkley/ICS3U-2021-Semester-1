49901407259629c8f98b3e4634a7fd2da6489a13
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-09-27 13:38:46 -0400

    Pool project, getVolume

---
+++ Unit Two - Using Objects/src/week4/BayviewGlenPools.java
@@ -0,0 +1,35 @@
+package week4;
+
+public class BayviewGlenPools {
+    public static void main(String[] args) {
+        final int length = 20;
+        final int shallowLength = 5;
+        final int transition = 7;
+        final int shallowHeight = 3;
+        final int deepHeight = 8;
+        final int width = 8;
+
+        final int linerCost = 2;
+
+        double surfaceArea = getSurfaceArea(length, width, shallowHeight, shallowLength, transition, deepHeight);
+        double volume = getVolume(length, width, shallowHeight, shallowLength, transition, deepHeight);
+
+    }
+
+    private static double getVolume(int length, int width, int shallowHeight, int shallowLength, int transition, int deepHeight) {
+        double shallowArea = shallowHeight * shallowLength;
+        double transitionHeight = deepHeight - shallowHeight;
+        double transitionLength = Math.sqrt(Math.pow(transition, 2) - Math.pow(transitionHeight, 2));
+        double deepArea = deepHeight * (length - (shallowLength + transitionLength));
+        double transitionArea = shallowHeight * transitionLength + (transitionLength * transitionHeight / 2);
+        double volume = (deepArea + transitionArea + shallowArea) * width;
+        return volume;
+    }
+
+    private static double getSurfaceArea(int length, int width, int shallowHeight, int shallowLength, int transition,
+            int deepHeight) {
+        
+    }
+
+
+}
