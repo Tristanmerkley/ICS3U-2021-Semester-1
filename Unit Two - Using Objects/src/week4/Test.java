91723148c9903f02031a4175a65b2d91ac04ff1b
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-10-04 13:25:50 -0400

    testing

---
+++ Unit Two - Using Objects/src/week4/Test.java
@@ -0,0 +1,30 @@
+package week4;
+
+public class Test {
+    public static void main(String[] args) {
+       int x = 3;
+
+int y = -1;
+
+if (x - 2 > y)
+
+{
+
+x -= y;
+
+}
+
+if (y + 3 >= x)
+
+{
+
+y += x;
+
+}
+
+System.out.print("x = " + x + " y = " + y);
+    }
+    
+      
+      
+}
