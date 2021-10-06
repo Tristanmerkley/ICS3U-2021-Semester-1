bce2e94fc92ee1dc785077de3352cec59f838de8
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-10-06 10:52:29 -0400

    Completed project, refined commenting

--- Unit Two - Using Objects/src/week5/CrossCountry.java
+++ Unit Two - Using Objects/src/week5/CrossCountry.java
@@ -5,9 +5,9 @@ import java.util.Scanner; // imports the java utility Scanner
 public class CrossCountry {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); // initiates keyboard input
-        processRunner(in); // runs processRunner
-        processRunner(in); // runs processRunner
-        processRunner(in); // runs processRunner
+        processRunner(in);
+        processRunner(in);
+        processRunner(in);
 
         in.close(); // ends keyboard input
     }
@@ -21,19 +21,19 @@ public class CrossCountry {
         String splitTwo, splitThree; // declares the Strings splitTwo, splitThree
 
 
-        firstName = promptUser("Please enter your first name: ", in); // calls promptUser and requests user's first name
-        lastName = promptUser("Please enter your last name: ", in); // calls promptUser and requests user's last name
-        mileOne = promptUser("Please enter mile one: ", in); // calls promptUser and requests user's first mile
-        mileTwo = promptUser("Please enter mile two: ", in); // calls promptUser and requests user's second mile
-        finishTime = promptUser("Please enter finish time: ", in); // calls promptUser and requests user's finish time
+        firstName = promptUser("Please enter your first name: ", in);
+        lastName = promptUser("Please enter your last name: ", in);
+        mileOne = promptUser("Please enter mile one: ", in);
+        mileTwo = promptUser("Please enter mile two: ", in);
+        finishTime = promptUser("Please enter finish time: ", in);
 
-         splitTwo = subtractTimes(mileTwo, mileOne); // sets splitTwo equal to a call for subtractTimes
-         splitThree = subtractTimes(finishTime, mileTwo); // sets splitThree equal to a call for subtractTimes
+         splitTwo = subtractTimes(mileTwo, mileOne);
+         splitThree = subtractTimes(finishTime, mileTwo);
 
-         System.out.println(lastName + " " + firstName + ":"); // prints on a new line
-         System.out.println("Split one: " + mileOne); // prints on a new line
-         System.out.println("Split two: " + splitTwo); // prints on a new line
-         System.out.println("Split three: " + splitThree); // prints on a new line
+         System.out.println(lastName + " " + firstName + ":");
+         System.out.println("Split one: " + mileOne);
+         System.out.println("Split two: " + splitTwo);
+         System.out.println("Split three: " + splitThree);
  
     }
     /**
@@ -43,8 +43,8 @@ public class CrossCountry {
      * @return
      */
 private static String promptUser(String prompt, Scanner in) {
-    System.out.print(prompt); // prints the inputed prompt
-    return in.nextLine(); // returns the users input
+    System.out.print(prompt);
+    return in.nextLine();
 }
 /**
  * Takes two times as string in the format of mm:ss.sss and returns the difference
@@ -53,12 +53,12 @@ private static String promptUser(String prompt, Scanner in) {
  * @return
  */
     private static String subtractTimes(String endTime, String startTime) {
-        double endInSeconds = convertToSeconds(endTime); // sets endInSeconds equal to a call for convertToSeconds inserting endTime
-        double startInSeconds = convertToSeconds(startTime); // sets startInSeconds equal to a call for convertToSeconds inserting startTime
+        double endInSeconds = convertToSeconds(endTime);
+        double startInSeconds = convertToSeconds(startTime);
 
-        double diffInSeconds = endInSeconds - startInSeconds; // inputs the difference between endTime and startTime into diffInSeconds
+        double diffInSeconds = endInSeconds - startInSeconds;
 
-        return convertToTime(diffInSeconds); // returns a call for convertToTime and inserts diffInSeconds
+        return convertToTime(diffInSeconds);
     }
 /**
  * Takes the total seconds as a double and returns it in the format mm:ss.sss
@@ -66,7 +66,7 @@ private static String promptUser(String prompt, Scanner in) {
  * @return
  */
     private static String convertToTime(double seconds) {
-        return String.format("%d:%06.3f", getMinutes(seconds), getSeconds(seconds)); // returns the time with the formatting mm:ss.sss
+        return String.format("%d:%06.3f", getMinutes(seconds), getSeconds(seconds));
     }
 
 /**
@@ -75,8 +75,8 @@ private static String promptUser(String prompt, Scanner in) {
  * @return
  */
     private static int getMinutes(Double totalSeconds) {
-        int minutes = (int)(totalSeconds/60); // turns totalSeconds into whole minutes
-        return minutes; // returns minutes
+        int minutes = (int)(totalSeconds/60);
+        return minutes;
     }
 /**
  * Takes the total seconds as a double and returns the remainder of seconds after whole minutes are taken out
@@ -84,8 +84,8 @@ private static String promptUser(String prompt, Scanner in) {
  * @return
  */
     private static double getSeconds(Double totalSeconds) {
-        double seconds = (totalSeconds % 60); // takes the remainder of totalSeconds after getting whole minutes
-        return seconds; // returns seconds
+        double seconds = (totalSeconds % 60);
+        return seconds;
     }
 /**
  * Takes a String in the format mm:s.sss and returns it as seconds forma: sss.sss
@@ -93,10 +93,10 @@ private static String promptUser(String prompt, Scanner in) {
  * @return
  */
     private static double convertToSeconds(String time) {
-        int colon = time.indexOf(":"); // gets index number of the colon
-        double minutes = Double.valueOf(time.substring(0, colon));  // creates a double called seconds and sets it equal to time starting 0 all the way to the index, colon, then it changes it from a String to a double
-        double seconds = Double.valueOf(time.substring(colon + 1)); // creates a double called seconds and sets it equal to time starting at the index, colon, then it changes it from a String to a double
-        return (minutes * 60) + seconds; // returns minutes in seconds and adds seconds
+        int colon = time.indexOf(":");
+        double minutes = Double.valueOf(time.substring(0, colon));
+        double seconds = Double.valueOf(time.substring(colon + 1));
+        return (minutes * 60) + seconds;
     }
 
 
