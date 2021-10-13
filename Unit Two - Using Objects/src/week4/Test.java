9cffd9ce0c9ce4eb2a9a39f3c002328d1ac83d0f
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-10-13 13:05:05 -0400

    Tests

--- Unit Two - Using Objects/src/week4/Test.java
+++ Unit Two - Using Objects/src/week4/Test.java
@@ -2,7 +2,21 @@ package week4;
 
 public class Test {
     public static void main(String[] args) {
-       
+      catDog("catdog");
+    }
+    public static boolean catDog(String str) {
+      int index = 0;
+      int cats = 0;
+      int dogs = 0;
+      while(index < str.length()-2){
+        if(str.substring(index, index+3).equals("cat")){
+          cats += 1;
+        }
+        if(str.substring(index, index+3).equals("dog")){
+          dogs += 1;
+        }
+      }
+      return cats == dogs;
     }
     
 }
