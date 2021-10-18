d7154ed3568b19a7ae789b681dbca2c10cb6c6e0
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-10-18 12:35:31 -0400

    Testing

--- Unit Two - Using Objects/src/week4/Test.java
+++ Unit Two - Using Objects/src/week4/Test.java
@@ -2,16 +2,13 @@ package week4;
 
 public class Test {
     public static void main(String[] args) {
-      //catDog("catdog");
-      int numRounds = 5;
-      int value = 9;
-      int random = (int)(Math.random()*10);
-      for (int i = 0; i < numRounds; i++){
-      while (random != value){
-      random = (int)(Math.random()*10);
-      System.out.print(random);
+      System.out.println(!( !( a && b ) || ( c || !d )));
     }
-    System.out.println();
+    public static boolean mystery(String str){
+      String temp = "";
+      for(int k = str.length();k>0;k--){
+        temp = temp + str.substring(k -1, k);
+      }
+      return temp.equals(str);
+    }
   }
-}
-}
