26f3e53af02b19902cb0026cb0a304d86d13e8ff
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-10-27 12:54:33 -0400

    Constructor + equals work

--- Unit Five and Nine - Creating Classes and Inheritance/src/week8/Die.java
+++ Unit Five and Nine - Creating Classes and Inheritance/src/week8/Die.java
@@ -8,7 +8,7 @@ public class Die {
     private int numSides;
     private int topSide;
 
-    //constructor
+    // constructor
     // has the same name as the class
     // you should create at least 1
     // they are used to obtain (construct an instance of this class)
@@ -20,18 +20,33 @@ public class Die {
         roll();
     }
 
-    public Die(int numSides){
+    public Die(int numSides) {
         /* the word this refers to the Object that we are working with */
         // the object the called this
         this.numSides = numSides;
         roll();
     }
 
-    public void roll(){
-        topSide = (int)(Math.random() * numSides) + 1;
+    public void roll() {
+        topSide = (int) (Math.random() * numSides) + 1;
     }
 
-    public int getTopSide(){
+    public int getTopSide() {
         return this.topSide;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+
+        if (obj instanceof Die) {
+            return this.topSide == ((Die) obj).topSide;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "" + topSide;
     }
 }
--- Unit Five and Nine - Creating Classes and Inheritance/src/week8/DieGame.java
+++ Unit Five and Nine - Creating Classes and Inheritance/src/week8/DieGame.java
@@ -4,11 +4,14 @@ public class DieGame {
     public static void main(String[] args) {
         Die die1 = new Die();
         Die die2 = new Die();
+
         int count = 0;
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             die1.roll();
             die2.roll();
-            if(die1.getTopSide() == die2.getTopSide())
+
+            System.out.println(die1 + " " + die2);
+            if (die1.equals(die2))
                 count++;
         }
         System.out.println("Pairs: " + count);
