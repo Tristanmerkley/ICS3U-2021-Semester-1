7e3495dcc487456c5ca48daeb7f88ed61558e2c2
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-11-16 12:50:06 -0500

    Insertion Sort Algorithm

--- Unit Six - Array and Array Lists/src/week11/SortingAlgorithms.java
+++ Unit Six - Array and Array Lists/src/week11/SortingAlgorithms.java
@@ -3,7 +3,19 @@ package week11;
 public class SortingAlgorithms {
     public static void main(String[] args) {
         int[] arr = {80, 70, 10, 15, 5, 90, 85};
-        selectionSort(arr);
+        //selectionSort(arr);
+        insertionSort(arr);
+    }
+
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j > 0 && temp < arr[j - 1]; j--) { // shift
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = temp;
+        }
     }
 
     private static void selectionSort(int[] arr) {
